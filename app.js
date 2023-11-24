8f345e79535e458bab978efb2b7ea9f592c3d648
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-11-24 22:55:55 +0100

    latestsave

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+// Ori Homepage
+app.get('/home',  (request, response) => {
+    console.log(__dirname)
+    response.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`)
+})
+
+app.get('/series', (req, res) => {
+    res.sendFile(`${__dirname}/views/series.html`)
+})
+
+app.get('/photogallery', (req, res) => {
+    res.sendFile(`${__dirname}/views/photogallery.html`)
+})
+
+app.listen(port, () => {
+    console.log(`My very first app is listening to port ${port}!`)
+});
+
