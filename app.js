de0b3e35ec8e7237fe4bd682f98120ac7d293369
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-11-25 12:38:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('noda app.jspublic'))
 
 // Ori Homepage
 app.get('/home',  (request, response) => {
